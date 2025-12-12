7d38e3c415e064f7bfec05d5064ec67b1f3da1f1
Punyapal Shah <mrpunyapal@gmail.com>
2025-12-12 17:51:47 +0530

    lint-refactor

--- tests/Rules/UseToHaveKeyRector/UseToHaveKeyRectorTest.php
+++ tests/Rules/UseToHaveKeyRector/UseToHaveKeyRectorTest.php
@@ -4,12 +4,10 @@ declare(strict_types=1);
 
 use Rector\Testing\Fixture\FixtureFileFinder;
 
-beforeAll(function () {
+beforeAll(function (): void {
     self::$configFilePath = __DIR__ . '/config/configured_rule.php';
 });
 
-test('', function (string $filePath) {
+test('', function (string $filePath): void {
     $this->doTestFile($filePath);
-})->with(function (): Iterator {
-    return FixtureFileFinder::yieldDirectory(__DIR__ . '/Fixture');
-});
+})->with(fn (): Iterator => FixtureFileFinder::yieldDirectory(__DIR__ . '/Fixture'));
