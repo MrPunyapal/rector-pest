2325f4d05d08f50d74387b6cbfd18a2adde4d0f7
Punyapal Shah <mrpunyapal@gmail.com>
2026-05-16 00:24:01 +0530

    lint

--- config/config.php
+++ config/config.php
@@ -9,7 +9,7 @@ use Rector\Config\RectorConfig;
  * Import this in your set configurations
  */
 return static function (RectorConfig $rectorConfig): void {
-    $configPath= "vendor".DIRECTORY_SEPARATOR."mrpunyapal".DIRECTORY_SEPARATOR."peststan".DIRECTORY_SEPARATOR."extension.neon";
+    $configPath = "vendor" . DIRECTORY_SEPARATOR . "mrpunyapal" . DIRECTORY_SEPARATOR . "peststan" . DIRECTORY_SEPARATOR . "extension.neon";
 
     if (file_exists($configPath)) {
         $rectorConfig->phpstanConfig($configPath);
